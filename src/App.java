511bcaf9f34f765abf8c3766d328cfd48edacca1
Abdullah Sawan <abdullahsawan19@gmail.com>
2025-10-15 01:34:24 +0300

    encapsulation

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
 public class App {
         public static void main(String[] args) throws Exception {
-        Car myCar = new Car( 120, "Red", "Model S", "Tesla", 2020 );
-        System.out.println(myCar.brand);              
-      
+        Car myCar = new Car(120, "Red", "Model S", "Tesla", 2020);
+
+        System.out.println(myCar); 
+
+        myCar.start();
+
+        myCar.setSpeed(150);
+        System.out.println("New speed: " + myCar.getSpeed());
+
     }
 }
