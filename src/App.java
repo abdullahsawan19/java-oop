73013a415ed7ec79e77e4dbfc296a5dc363b04a1
Abdullah Sawan <abdullahsawan19@gmail.com>
2025-10-14 18:17:12 +0300

    constructor

--- src/App.java
+++ src/App.java
@@ -1,9 +1,7 @@
 public class App {
         public static void main(String[] args) throws Exception {
-        Car myCar = new Car();
-        myCar.start();
-        myCar.speed = 100;
-        System.out.println("Car speed is: " + myCar.speed);
-        System.out.println("Car color is: " + myCar.color);
+        Car myCar = new Car( 120, "Red", "Model S", "Tesla", 2020 );
+        System.out.println(myCar.brand);              
+      
     }
 }
--- src/Car.java
+++ src/Car.java
@@ -5,6 +5,18 @@ public class Car {
     String brand;
     int year;
 
+
+    
+
+    public Car(
+        int speed, String color, String model, String brand, int year
+    ){
+        this.speed = speed;
+        this.color = color;
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+    }
    void start(){
     System.out.println("Car is starting");
    }
